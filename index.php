16b711b4077108941d98d76759112550ff7d1ec7
Mark Horton <mark.horton@totallyboundless.com>
2014-10-30 11:09:14 +0000

    Ripped code from existing holding page

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Victoria’s Little Bra Shop</title>
+<link href="reset.css" rel="stylesheet" type="text/css" />
+<link href="main_vlbs3.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="wrapper">
+	<div id="containerNew"><img src="images-victoria/Victoria's-little-bra-shop3.png" alt="Victoria's little Bra Shop" width="829" height="690" border="0" usemap="#Map" />
+      <map name="Map" id="Map">
+        <area shape="rect" coords="220,514,607,544" href="mailto:info@victoriaslittlebrashop.com" alt="Contact us by email" />
+      </map>
+	</div>
+    </div><!-- End of wrapper-->
+</body>
+</html>
